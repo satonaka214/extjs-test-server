57b1944e6d7fde320d34bf07f21f9924fb4d31cd
satonaka214 <spt85wx9@gmail.com>
2019-05-15 07:53:57 +0900

    first commit

---
+++ src/main/java/com/satonaka/extjstestserver/ExtjsTestServerApplication.java
@@ -0,0 +1,13 @@
+package com.satonaka.extjstestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtjsTestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtjsTestServerApplication.class, args);
+	}
+
+}
